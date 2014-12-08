03d7c9208ecf08c59007aaf09aa227ce79a9c06c
JonathanTomaske <jonathan.tomaske@gmail.com>
2014-12-07 23:37:05 -0800

    Update IssueController.cs
    Started making IssueController

--- Assets/Scripts/IssueController.cs
+++ Assets/Scripts/IssueController.cs
@@ -2,10 +2,12 @@
 using System.Collections;
 
 public class IssueController : MonoBehaviour {
+	
+	private List<Issue> Issues = new List<Issue>();
 
 	// Use this for initialization
 	void Start () {
-	
+		issues.add(new Issue("Farm Disaster", 1))
 	}
 	
 	// Update is called once per frame
