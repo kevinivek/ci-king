fb56924a0ad2ec5fe3d9cb1f1cf856dd14c54189
kevinivek <kevin.weeks@comcast.net>
2014-12-07 22:51:43 -0800

    Update Issue.cs

--- Assets/Scripts/Issue.cs
+++ Assets/Scripts/Issue.cs
@@ -3,6 +3,21 @@ using System.Collections;
 
 public class Issue : MonoBehaviour {
 
+	public int[] statsModifier = {0, 0, 0, 0, 0, 0}; 
+	public string[] statNames = {"Fear", "Depression", "Joy", "Anger", "Apathy", "Hunger"}; 
+	public const int FEAR = 0;
+	public const int DEPRESSION = 1;
+	public const int JOY = 2;
+	public const int ANGER = 3;
+	public const int APATHY = 4;
+	public const int HUNGER = 5;
+	private String title = "";
+	private String description = "";
+	
+	public Issue(String title){
+		this.title = title;
+	}
+
 	// Use this for initialization
 	void Start () {
 	
@@ -12,4 +27,14 @@ public class Issue : MonoBehaviour {
 	void Update () {
 	
 	}
+	
+	public String getTitle(){
+		return this.title;
+	}
+	
+	public String getDescription(){
+		return this.description;
+	}
+	
+	
 }
