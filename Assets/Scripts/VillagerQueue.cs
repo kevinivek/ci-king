f43264b66ebf5d70050224390a7ab3d6921e17d4
Jonathan Tomaske <jonathan.tomaske@gmail.com>
2014-12-07 23:17:20 -0800

    Added in VillagerQueue class that manages the queue of villagers entering the Throne Room

---
+++ Assets/Scripts/VillagerQueue.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class VillagerQueue : MonoBehaviour {
+
+	private GameObject[] markers;
+	private List<GameObject> villagerInQueue = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+		GameObject[] tempMarkers = GameObject.FindGameObjectsWithTag(Tags.villagerMarker);
+		int numMarker = tempMarkers.Length;
+		markers = new GameObject[numMarker];
+		foreach(GameObject marker in tempMarkers ){
+			int id = marker.GetComponent<MarkerController>().id;
+			markers[id] = marker;
+		}
+
+		foreach(GameObject marker in markers) {
+			Debug.Log("marker: " + marker.GetComponent<MarkerController>().id);
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
