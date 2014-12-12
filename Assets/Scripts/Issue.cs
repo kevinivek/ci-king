d247f509979bfa51ce1a6f4b2d5f2aae5217e42e
Jonathan Tomaske <jonathan.tomaske@gmail.com>
2014-12-11 21:29:07 -0800

    Changed some stuff

--- Assets/Scripts/Issue.cs
+++ Assets/Scripts/Issue.cs
@@ -5,10 +5,10 @@ public class Issue{
 
 	public int[] statsModifier = {0, 0, 0, 0, 0, 0}; 
 	
-	private string title = "";
-	private string description = "";
-	private int numOptions = 0; //the number of options in this issue, starts with zero
-	private IssueOption[] options;
+	public string title = "";
+	public string description = "";
+	public int numOptions = 0; //the number of options in this issue, starts with zero
+	public IssueOption[] options;
 	
 	public Issue(string title, int num){
 		this.title = title;
@@ -18,14 +18,7 @@ public class Issue{
 	public void setDescription(string description){
 		this.description = description;
 	}
-	
-	public string getTitle(){
-		return this.title;
-	}
-	
-	public string getDescription(){
-		return this.description;
-	}
+
 	public void addOption(string oName, int[] stats, bool all){
 		options[numOptions] = new IssueOption(oName, numOptions + 1, stats, all);
 		numOptions++;
--- Assets/Scripts/IssueManager.cs
+++ Assets/Scripts/IssueManager.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 public class IssueManager : MonoBehaviour {
 	
-	private List<Issue> issues = new List<Issue>();
+	public List<Issue> issues = new List<Issue>();
 
 	// Use this for initialization
 	public void Init () {
@@ -15,36 +15,16 @@ public class IssueManager : MonoBehaviour {
 		issues[0].addOption("Have him beheaded for his insolence", new int[] {10, 5, 0, 5, 0, 0}, true); //stats will need to affect all other villagers
 
 		issues.Add (new Issue ("Missing Child", 3));
-		issues [1].setDescription ("My daughter went down to play in the river, and she never came back, we can't find her anywhere around town, will you help us?");
-		issues [1].addOption ("Send help right away", new int[] {-10, -10, 10, 0, 0, 0}, false);  //subtract money from the coffers
+		issues[1].setDescription ("My daughter went down to play in the river, and she never came back, we can't find her anywhere around town, will you help us?");
+		issues[1].addOption ("Send help right away", new int[] {-10, -10, 10, 0, 0, 0}, false);  //subtract money from the coffers
 		issues[1].addOption("Find the child, but force her into labor at the castle", new int[] {0, 10, -10, 10, 10, 0}, false);
 		issues[1].addOption("Have him beheaded for suggesting you stoop to helping him", new int[] {10, 5, 0, 5, 0, 0}, true); //stats will need to affect all other villagers
 
 		issues.Add (new Issue ("Lower Taxes", 3));
-		issues [2].setDescription ("The taxes are so high as to be crippling sire, will you please show some mercy?");
-		issues [2].addOption ("Lower the taxes", new int[] {0, -10, 10, 0, -10, 0}, false);
-		issues [2].addOption ("Raise the taxes", new int[] {0, 10, 0, 5, 10, 0}, false);
+		issues[2].setDescription ("The taxes are so high as to be crippling sire, will you please show some mercy?");
+		issues[2].addOption ("Lower the taxes", new int[] {0, -10, 10, 0, -10, 0}, false);
+		issues[2].addOption ("Raise the taxes", new int[] {0, 10, 0, 5, 10, 0}, false);
 		issues[2].addOption("Have him beheaded for his insolence", new int[] {10, 5, 0, 5, 0, 0}, true); //stats will need to affect all other villagers
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
-	public Issue getIssue()
-	{
-		return issues [issues.Count];
-	}
-
-	void removeIssue()
-	{
-
-	}
-
-	public Issue getIssue(int pos)
-	{
-		return issues [pos];
 	}
 
 }
