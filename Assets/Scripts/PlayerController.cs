8f6dab9f98577e80b41f977b70fa84d767d3bc37
Jonathan Tomaske <jonathan.tomaske@gmail.com>
2014-12-11 20:34:48 -0800

    Added gold and food to player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,45 +3,18 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	public GameObject villager;
+	public int gold;
+	public int food;
 
 	// Use this for initialization
 	void Start () {
+		gold = 200;
+		food = 200;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-//		Vector3 fwd = transform.TransformDirection (Vector3.forward);
-//		RaycastHit hit;
-//
-//		GameObject cam = GameObject.FindGameObjectWithTag ("MainCamera");
-//
-//		Debug.DrawRay (cam.transform.position, fwd, Color.red);
-//
-//		HUDController HUDCont = villagerHUD.gameObject.GetComponent<HUDController>();
-//
-//		if(Physics.Raycast(cam.transform.position, fwd, out hit)) {
-//			if(hit.collider.gameObject.CompareTag(Tags.villager)) {
-//				if(!HUDCont.activeHUD) {
-//					HUDCont.villager = hit.collider.gameObject;
-//					HUDCont.setPosition();
-//				}
-//			}
-//			else {
-//				if(HUDCont.activeHUD) {
-//					HUDCont.villager = null;
-//					HUDCont.resetPosition();
-//				}
-//			}
-//		}
 	
 	}
 
-	void OnControllerColliderHit(ControllerColliderHit hit) {
-		if (hit.gameObject.CompareTag (Tags.villager)) {
-			VillagerController villCont = hit.gameObject.GetComponent<VillagerController>();
-			villCont.stats[ VillagerStats.HUNGER ] += 10;
-		}
-	}
 }
