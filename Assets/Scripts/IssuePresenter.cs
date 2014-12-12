2026d969ad738564d760f07d5943a66f6b80c90d
Jonathan Tomaske <jonathan.tomaske@gmail.com>
2014-12-11 19:34:46 -0800

    Stats and Issue HUDs now created by IssuePresenter, positions calculated dynamically to fit around the villager and front marker.

--- Assets/Scripts/IssuePresenter.cs
+++ Assets/Scripts/IssuePresenter.cs
@@ -5,17 +5,41 @@ public class IssuePresenter : MonoBehaviour {
 
 	public GameObject HUDPrefab;
 
-	private GameObject HUD;
+	private GameObject HUDStats;
+	private GameObject HUDIssue;
+
+	public VillagerQueue villagerQueue; 
 
 	// Use this for initialization
 	public void Init() {
-		HUD = Instantiate (HUDPrefab) as GameObject;
-		HUDController HUDC = HUD.GetComponent<HUDController> ();
-		HUDC.Init();
-		HUDC.setPosition(new Vector3(0, 4, -1.5f));
-		HUDC.HUDtext.fontSize = 12;
-		HUDC.setText("Testing...1..2..3");
-		HUDC.resize(new Vector3 (2, 1, 1));
+		HUDStats = Instantiate (HUDPrefab) as GameObject;
+		HUDController HUDS = HUDStats.GetComponent<HUDController> ();
+		HUDS.Init();
+		HUDS.setPosition(new Vector3(0, 4, -1.5f));
+		HUDS.HUDtext.fontSize = 12;
+		HUDS.setText("Testing...1..2..3");
+		HUDS.resize(new Vector3 (0.5f, 1, 1));
+		
+		HUDIssue = Instantiate (HUDPrefab) as GameObject;
+		HUDController HUDI = HUDIssue.GetComponent<HUDController> ();
+		HUDI.Init();
+		HUDI.setPosition(new Vector3(0, 4, -1.5f));
+		HUDI.HUDtext.fontSize = 12;
+		HUDI.setText("Testing...1..2..3");
+		HUDI.resize(new Vector3 (1, 0.5f, 1));
+		
+		Vector3 markerPos = villagerQueue.markers[0].transform.position;
+
+		Vector3 HUDSB = HUDS.renderer.bounds.size;
+		Vector3 VillB = villagerQueue.villagerInQueue[0].renderer.bounds.size;
+		Vector3 newPos = new Vector3(markerPos.x - (HUDSB.x/2+VillB.x/2), HUDSB.y/2 + markerPos.y, HUDSB.z/2 + markerPos.z);
+		HUDS.setPosition(newPos);
+
+		Vector3 HUDIB = HUDI.renderer.bounds.size;
+		newPos = new Vector3(markerPos.x + (HUDIB.x/2+VillB.x/2), HUDIB.y/2 + markerPos.y, HUDIB.z/2 + markerPos.z);
+		HUDI.setPosition(newPos);
+
+
 	}
 	
 	// Update is called once per frame
